494db2e823e7e1362505bba8f46079b76015bb12
Adamu Muhammad <sayyid211@gmail.com>
2023-09-24 10:27:33 +0100

    weathermap api connected

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 import Tabs from "./src/components/Tabs"
 import { NavigationContainer } from "@react-navigation/native";
-import Counter from "./src/components/demonstration/Counter";
+import { useGetWeather } from "./src/hooks/useGetWeather";
 
 const App = () => {
+  const [loading, error, weather] = useGetWeather()
+  console.log(loading, error, weather)
+  
+  if (weather && weather.list) {
+    return (
+      <NavigationContainer>
+        <Tabs Weather={weather} />
+      </NavigationContainer>
+    )
+  }
   return (
-    <NavigationContainer>
-      <Counter />
-    </NavigationContainer>
+    <View style={styles.container}>
+      <ActivityIndicator  size={'large'} color={'blue'}/>
+    </View>
   )
+  
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  }
+})
 export default App;
--- src/components/demonstration/Counter.js
+++ src/components/demonstration/Counter.js
@@ -1,8 +1,16 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {Text, View, Button, StyleSheet} from "react-native";
 const Counter = () => {
     const {container, title} = styles;
     const [count, setCount] = useState(0);
+    const [newCount, setnewCount] = useState(0);
+
+    useEffect(() => {
+        console.log(`Count changed`)
+        return () => {
+            console.log(`useEffect cleanup`)
+        }
+    }, [count]);
     return (
         <View style={container}>
             <Text style={title}>{`Count: ${count}`}</Text>
@@ -18,6 +26,21 @@ const Counter = () => {
                 title={"Decrease the count"}
                 onPress={() => {
                     setCount(count - 1)
+                }}
+            />
+            <Text style={title}>{`Count: ${newCount}`}</Text>
+            <Button
+                color = {'red'} 
+                title={"Increase the count"}
+                onPress={() => {
+                    setnewCount(newCount + 1)
+                }}
+            />
+            <Button
+                color = {'green'} 
+                title={"Decrease the count"}
+                onPress={() => {
+                    setnewCount(newCount - 1)
                 }}
             />
         </View>
